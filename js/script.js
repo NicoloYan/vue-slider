30d55db928e0d2cecbcbfd374fc3fa75260994ae
NicoloYan <nico.yan@outlook.it>
2022-06-24 15:45:36 +0200

    Excercise complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var app = new Vue (
         el: '#app',
         data: {
 
+            currentActiveElement: 0,
             slides: [
                 {
                     image: 'img/01.jpg',
@@ -30,6 +31,28 @@ var app = new Vue (
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
                 }
             ]
+        },
+        methods: {
+            showNextElement() {
+                // Incrementa il valore di currentActiveElement fino ad arrivare alla fine dell'array.
+                // Quando si arriva alla fine, fa tornare all'inizio dell'array
+                if(this.currentActiveElement < this.slides.length - 1) {
+                    this.currentActiveElement++;                    
+                } else {
+                    this.currentActiveElement = 0
+                }
+
+            },
+            showPrevElement() {
+                // Decrementa il valore di currentActiveElement fino ad arrivare all'inizio dell'array.
+                // Quando si arriva all'inizio, fa tornare alla fine dell'array
+                if(this.currentActiveElement > 0) {
+                    this.currentActiveElement--;                    
+                } else {
+                    this.currentActiveElement = this.slides.length - 1
+                }
+
+            }
         }
     }
 );
